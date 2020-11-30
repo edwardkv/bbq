ce8fcffcbe7700674b03a29aebe9b5ba59d51584
EdwardKV <edwardkv@gmail.com>
2020-11-30 11:10:52 +0400

    user to event reference

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
+  # Событие принадлежит юзеру
+  belongs_to :user
+
   # Валидируем заголовок, он не может быть длиннее 255 букв
-  validates :title, presence: true, length: { maximum: 255 }
+  validates :title, presence: true, length: {maximum: 255}
   # У события должны быть заполнены место и время
   validates :address, presence: true
   validates :datetime, presence: true
