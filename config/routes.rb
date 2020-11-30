14f5cfb69fdad0a95eb164365b66ce709a4df2ba
EdwardKV <edwardkv@gmail.com>
2020-11-30 10:52:35 +0400

    users initial

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root "events#index"
 
   resources :events
+
+  resources :users, only: [:show, :edit, :update]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
