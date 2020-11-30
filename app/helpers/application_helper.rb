139f0eef2be59ac11437a9ad6ba400e4a4ad6855
edwardkv <edwardkv@gmail.com>
2020-11-30 21:39:17 +0400

    Install bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+
+  def bootstrap_flash(opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: "alert") do
+        concat content_tag(:button, '×', class: "close", data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
+  private
+  def bootstrap_class_for(level)
+    case level
+    when "notice" then "alert-info"
+    when "success" then "alert-success"
+    when "error" then "alert-error"
+    when "alert" then "alert-error"
+    else level
+    end
+  end
+
 end
