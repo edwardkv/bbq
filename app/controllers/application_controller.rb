03e0a9d2738ebb3332c881423c81f9944ae0dc99
edwardkv <edwardkv@gmail.com>
2020-12-03 20:12:47 +0400

    fix name in register form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,12 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_can_edit?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-        :account_update,
-        keys: [:password, :password_confirmation, :current_password]
-    )
+
+    devise_parameter_sanitizer.permit(:account_update,
+                                       keys: [:password, :password_confirmation, :current_password, :name])
+
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 
   def current_user_can_edit?(event)
