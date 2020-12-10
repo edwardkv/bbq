0f4416c5d8de3494f9632cdaf415fa8256d368ad
edwardkv <edwardkv@gmail.com>
2020-12-10 10:19:08 +0400

    Improve views, add helper current_user_owns?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :current_user_can_edit?
+  helper_method :current_user_owns?
 
   def configure_permitted_parameters
 
@@ -20,6 +21,10 @@ class ApplicationController < ActionController::Base
       model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
     )
+  end
+
+  def current_user_owns?(event)
+    event.user == current_user
   end
 
   def default_url_options
