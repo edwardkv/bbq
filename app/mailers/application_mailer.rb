f02279784c95e68645bfcaea1be69eff8d7ae07a
edwardkv <edwardkv@gmail.com>
2020-12-13 16:47:27 +0400

    Send email: try from email heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'edwardprogrammer69@gmail.com'
+  #default from: 'edwardprogrammer69@gmail.com'
+  default from: 'app192931049@heroku.com'
   layout 'mailer'
 end
