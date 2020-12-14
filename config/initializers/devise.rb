0317102012bae433bbee45ce94a33763363bcbdb
edwardkv <edwardkv@gmail.com>
2020-12-14 10:36:05 +0400

    restore password config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,6 +309,8 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   #
-  ActionMailer::Base.default_url_options = { :host => "localhost:3000" }
+  #ActionMailer::Base.default_url_options = { :host => "localhost:3000" }
+
+  ActionMailer::Base.default_url_options =  { :host => 'edward-letsgo.herokuapp.com' }
   #config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
