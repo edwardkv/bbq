5c7f500e288c0ff471ac651b35437455df003c39
edwardkv <edwardkv@gmail.com>
2021-01-10 12:54:49 +0400

    clear code

--- app/jobs/mail_delivery_job.rb
+++ app/jobs/mail_delivery_job.rb
@@ -3,14 +3,13 @@ class MailDeliveryJob < ApplicationJob
 
   def perform(event, entity, mail='')
     name_of_entity = entity.class.to_s
-
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [entity&.user&.email]).uniq
 
     case name_of_entity
     when "Comment"
       all_emails.each { |mail| EventMailer.comment(event, entity, mail).deliver_later }
     when "Photo"
-      all_emails.each { |mail|  EventMailer.photo(event, entity, mail).deliver_later }
+      all_emails.each { |mail| EventMailer.photo(event, entity, mail).deliver_later }
     when "Subscription"
       EventMailer.subscription(event, entity).deliver_later
     end
